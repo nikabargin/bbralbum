d48ed42267cfe115144d1b42b9455d8b99456b0d
nikabargin <nikabargin@gmail.com>
2018-09-18 14:21:39 +0300

    ''

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2018_09_07_105023) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,23 +33,23 @@ ActiveRecord::Schema.define(version: 2018_09_07_105023) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "colors", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "hardnesses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "hardnesses", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "skins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "skins", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "type_id"
-    t.bigint "color_id"
-    t.bigint "target_id"
-    t.bigint "hardness_id"
+    t.integer "type_id"
+    t.integer "color_id"
+    t.integer "target_id"
+    t.integer "hardness_id"
     t.string "sname"
     t.string "video"
     t.index ["color_id"], name: "index_skins_on_color_id"
@@ -58,15 +58,15 @@ ActiveRecord::Schema.define(version: 2018_09_07_105023) do
     t.index ["type_id"], name: "index_skins_on_type_id"
   end
 
-  create_table "targets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "targets", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "types", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -78,8 +78,4 @@ ActiveRecord::Schema.define(version: 2018_09_07_105023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "skins", "colors"
-  add_foreign_key "skins", "hardnesses"
-  add_foreign_key "skins", "targets"
-  add_foreign_key "skins", "types"
 end
