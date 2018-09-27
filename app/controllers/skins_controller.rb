e36804f9da09ce782c77945a2d25ca81f824328d
nikabargin <nikabargin@gmail.com>
2018-09-27 11:11:34 +0300

    ''

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -13,7 +13,7 @@ class SkinsController < ApplicationController
         with_target: Target.options_for_select
       }
     ) or return
-    @skins = @filterrific.find.page(params[:page]).per_page(6)
+    @skins = @filterrific.find.page(params[:page]).per_page(12)
 
 
 
